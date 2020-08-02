d522493b7a6986bc80ca787c66c26b90bea6c022
AbdelRahman Emam <abdoemamofficial@gmail.com>
2020-08-02 16:29:49 +0200

    first deploy

--- src/pages/Profilepage/Profilepage.js
+++ src/pages/Profilepage/Profilepage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import classes from "./Profilepage.module.css";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { setCurrentPage } from "../../features/currentPageSlice";
 import Navbar from "../../containers/Navbar/Navbar";
 import Footer from "../../containers/Footer/Footer";
@@ -9,6 +9,9 @@ import Layout from "../../hoc/Layout/Layout";
 const Profilepage = (props) => {
   const sidebarArr = ["General info", "Previous workspaces", "Change my password", "Delete my account"];
   const [activePanel, setActivePanel] = useState(0);
+  const [changeName, setChangeName] = useState(false);
+  const [changeEmail, setChangeEmail] = useState(false);
+  const { userData } = useSelector((state) => state);
   const dispatch = useDispatch(setCurrentPage);
   useEffect(() => {
     dispatch(setCurrentPage("Profile"));
@@ -17,6 +20,14 @@ const Profilepage = (props) => {
 
   const panelClickHandler = (index) => {
     setActivePanel(index);
+  };
+
+  const changeClickHandler = (type) => {
+    if (type === "name") {
+      setChangeName(true);
+    } else if (type === "email") {
+      setChangeEmail(true);
+    }
   };
 
   return (
@@ -36,7 +47,35 @@ const Profilepage = (props) => {
               ))}
             </ul>
           </div>
-          <div className={classes.mainArea}>Hello</div>
+          <div className={classes.mainArea}>
+            <div className={classes.mainAreaContainer}>
+              <div className={classes.mainAreaRow}>
+                <label htmlFor="name">Name:</label>
+                <input type="text" placeholder={userData.name} disabled={!changeName} />
+                {changeName ? (
+                  <div className={classes.icons}>
+                    <i className="fa fa-check"></i>
+                    <i className="fa fa-times"></i>
+                  </div>
+                ) : (
+                  <p className={classes.changeBtn} onClick={() => changeClickHandler("name")}>
+                    change
+                  </p>
+                )}
+              </div>
+              <div className={classes.mainAreaRow}>
+                <label htmlFor="name">Email:</label>
+                <input type="text" placeholder={userData.email} disabled={true} />
+                <p className={classes.changeBtn} onClick={() => changeClickHandler("email")}>
+                  change
+                </p>
+              </div>
+              <div className={classes.mainAreaRow}>
+                <p>Creation date:</p>
+                <p>20/07/2020</p>
+              </div>
+            </div>
+          </div>
         </div>
       </Layout>
       <div className={classes.footer}>
